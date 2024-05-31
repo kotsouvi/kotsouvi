058fa4f0d18213bd6413c11129e3a645c65f4de2
kotsouvi <168630105+kotsouvi@users.noreply.github.com>
2024-05-31 13:18:47 +0300

    Add files via upload

--- students.py
+++ students.py
@@ -6,16 +6,16 @@ def add_student(students):
 	students[name] = list(map(int, grades))
 
 def display_students(students):
-	for name,grades in student.items():
-		print(f'{name}: {grades}")
+	for name, grades in students.items():
+		print(f"{name}: {grades}")
 
 def calculate_average(students):
-	for name,grades in students.items():
+	for name, grades in students.items():
 		avg=sum(grades) / len(grades)
 		print(f"{name}: {avg:.2f}")
 
 def find_top_student(students):
-	top_student = max(students, key=lanbda name: sum(students[name]) / len(students[name]))
+	top_student = max(students, key=lambda name: sum(students[name]) / len(students[name]))
 	print(f"Top student: {top_student} with average grade {sum(students[top_student]) / len(students[top_atudent]):.2f}")
 
 def search_student(students, name):
@@ -34,9 +34,49 @@ def load_from_file(filename):
 		with open(filename, 'r')as file:
 			students = json.load(file)
 		return students
-	exept FileNotFoundError :
+	except FileNotFoundError :
 		print("File not found.")
 		return {}
-	exept json.JSONDecodeError:
+	except json.JSONDecodeError:
 		print("Error decoding JSON from file.")
 		return {}
+		
+def main():
+    students = {} 
+    while True: 
+        print("\nMenu:") 
+        print("1. Add student") 
+        print("2. Display all students")
+        print("3. Caculate average grades") 
+        print("4. Find top student")
+        print("5. Search student by name") 
+        print("6. Save to file") 
+        print("7. Load from file")
+        print("8. Exit") 
+        
+        choice = input("Enter your choice: ")
+        
+        if choice == '1':
+            add_student(students) 
+        elif choice == '2':
+            display_students(students)
+        elif choice == '3':
+            calculate_average(students)
+        elif choice == '4':
+            find_top_student(students)
+        elif choice == '5':
+            name = input("Enter student's name to search:")
+            search_student(students, name)
+        elif choice == '6':
+            filename = input("Enter filename to save: ") 
+            save_to_file(students, filename)
+        elif choice == '7':
+            filename = input("Enter filename to save: ")
+            students = load_from_file(filename)
+        elif choice == '8':
+            break 
+        else:
+            print("Invalid choice. Pleace try again.")
+           
+if __name__ == "__main__":
+    main() 
